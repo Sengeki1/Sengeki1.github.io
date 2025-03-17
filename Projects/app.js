bb845422198624c9e8953aa742a0783a9b248719
Sengeki1 <gamersengeki@gmail.com>
2025-03-17 16:24:36 -0100

    finished

---
+++ Projects/app.js
@@ -0,0 +1,36 @@
+document.getElementById("btn1").addEventListener("click", function() {
+    window.location.href = "Runner/index.html";
+});
+
+document.getElementById("btn2").addEventListener("click", function() {
+    window.location.href = "Space Invaders/index.html";
+});
+
+document.getElementById("btn3").addEventListener("click", function() {
+    window.location.href = "Tetris/index.html";
+});
+
+document.getElementById("btn4").addEventListener("click", function() {
+    window.location.href = "Pong/index.html";
+});
+
+document.getElementById("btn5").addEventListener("click", function() {
+    window.location.href = "Breakout/index.html";
+});
+
+document.getElementById("btn6").addEventListener("click", function() {
+    window.location.href = "Platformer/index.html";
+});
+
+document.getElementById("btn7").addEventListener("click", function() {
+    window.location.href = "Fighter/index.html";
+});
+
+document.getElementById("btn8").addEventListener("click", function() {
+    window.location.href = "Shooter/index.html";
+});
+
+
+
+
+
